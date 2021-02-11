93255c1b495401566123d8d66815c8f70ad44b08
jfangwang <qbs1864@gmail.com>
2021-02-11 15:49:02 +0000

    added init, __str__, and save

---
+++ models/base_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Base Model File"""
+from datetime import datetime
+from uuid import uuid4
+
+class BaseModel:
+	"""Base Model"""
+		def __init__(self, id):
+	"""__init__"""
+	id = str(uuid.uuid4())
+	created_at = datetime.now()
+	updated_at = datetime.now()
+
+	def __str__(self):
+		"""__str__"""
+			return ("[{}] ({}) {}".format(self.__class__.name, self.id, self.__dict__))
+
+	def save(self):
+		"""save"""
+		self.updated_at = datetime.now()
