4a1520f4cef6991abd8142b380621a1d2a6a18e4
jfangwang <qbs1864@gmail.com>
2021-02-11 16:17:34 +0000

    stuck with unittesting

--- models/base_model.py
+++ models/base_model.py
@@ -4,17 +4,17 @@ from datetime import datetime
 from uuid import uuid4
 
 class BaseModel:
-	"""Base Model"""
-		def __init__(self, id):
-	"""__init__"""
-	id = str(uuid.uuid4())
-	created_at = datetime.now()
-	updated_at = datetime.now()
+    """Base Model"""
+    def __init__(self, id):
+        """__init__"""
+        self.id = str(uuid.uuid4())
+        created_at = datetime.now()
+        updated_at = datetime.now()
 
-	def __str__(self):
-		"""__str__"""
-			return ("[{}] ({}) {}".format(self.__class__.name, self.id, self.__dict__))
+    def __str__(self):
+        """__str__"""
+        return ("[{}] ({}) {}".format(self.__class__.name, self.id, self.__dict__))
 
-	def save(self):
-		"""save"""
-		self.updated_at = datetime.now()
+    def save(self):
+        """save"""
+        self.updated_at = datetime.now()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""unit test for base model"""
+import unittest
+from models.base_model import BaseModel
+
+class test_base_model(unittest.TestCase):
+    """Class test_base"""
+    @classmethod
+    def setUpClass(cls):
+        """Method set up instances"""
+        cls.mb1 = BaseModel()
+        print("mb1 created")
+
+    def test_id(self):
+        """test id"""
+        print("{}".format(self.mb1.id))
