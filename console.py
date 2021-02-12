e95484fbd23a3ca684a6a5d7678c1542b47a2fcf
jfangwang <qbs1864@gmail.com>
2021-02-12 17:09:39 +0000

    implemented cowsay

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from cmd import Cmd
+import cowsay
 
 class HBNBCommand(Cmd):
     """HBNBC"""
@@ -14,9 +15,18 @@ class HBNBCommand(Cmd):
     def emptyline(self):
         pass
 
-    def do_create(self):
+    def do_cowsay(self, arg):
+        """implementing cowsay test"""
+        cowsay.cow(arg)
+
+    def do_create(self, arg):
         """Creates a new instance"""
-        
+        if str(arg) is "base":
+            print("it works")
+        elif not arg:
+            print("** class name missing **")
+        else:
+            print("** class doesn't exist **")
         
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
